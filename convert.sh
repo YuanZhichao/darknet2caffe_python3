5c8c4390816118c82ab15e039258843e245d1e83
YuanZhichao <46312012+YuanZhichao@users.noreply.github.com>
2021-03-22 16:50:09 +0800

    Add files via upload

---
+++ convert.sh
@@ -0,0 +1,2 @@
+# python darknet2caffe.py [input].cfg [input].weights [output].prototxt [output].caffemodel
+python darknet2caffe.py yolov3-tiny.cfg yolov3-tiny.weights yolov3-tiny.prototxt yolov3-tiny.caffemodel
